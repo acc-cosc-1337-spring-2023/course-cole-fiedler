bb69f276d8fa69ff1ac8ca7b5088202a90329154
cole-fiedler <123602079+cole-fiedler@users.noreply.github.com>
2023-03-24 18:57:48 +0000

    tic tac toe class pub/priv

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,28 @@
 //h
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string; using std::vector;
+
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+class TicTacToe
+{
+public:
+    bool game_over();
+    void start_game(string first_player);
+    void mark_board(int position);
+    string get_player() const{return player;}
+    void display_board() const;
+private:
+    void set_next_player();
+    bool check_board_full();
+    void clear_board();
+    string player;
+    vector<string>peg ( 9 , " " );
+}
+
+#endif

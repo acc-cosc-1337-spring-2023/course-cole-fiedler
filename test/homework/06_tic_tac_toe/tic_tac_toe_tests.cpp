d1b782d1f54c92847cbfb1aa1540b6071205bee1
cole-fiedler <123602079+cole-fiedler@users.noreply.github.com>
2023-04-15 19:32:09 +0000

    TTT3 test cases

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -8,275 +8,269 @@ TEST_CASE("Verify Test Configuration", "verification") {
 
 TEST_CASE("Verify get_player returns proper value when X is passed as first player")
 {
-	TicTacToe game;
+	TicTacToe3 game;
 	std::string first_player = "X";
-	game.start_game(first_player);
-	REQUIRE(game.get_player() == "X"); // require player x
+	game.start_game_3(first_player);
+	REQUIRE(game.get_player_3() == "X"); // require player x
 }
 
 TEST_CASE("Verify get_player returns proper value when O is passed as first player")
 {
-	TicTacToe game;
+	TicTacToe3 game;
 	std::string first_player = "O";
-	game.start_game(first_player);
-	REQUIRE(game.get_player() == "O");  // require player O
+	game.start_game_3(first_player);
+	REQUIRE(game.get_player_3() == "O");  // require player O
 }
 
 TEST_CASE("Verify check board full executes properly and returns C in the result of a tie")
 {
 	// Verify full board ends game with winner "C"
-	TicTacToe game;
+	TicTacToe3 game;
 	std::string first_player = "X";
-	game.start_game(first_player);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(1);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(5);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(9);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(3);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(7);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(8);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(2);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(4);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(6);
-	REQUIRE(game.game_over() == true);
-	REQUIRE(game.get_winner() == "C"); // require c winner
+	game.start_game_3(first_player);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(1);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(5);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(9);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(3);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(7);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(8);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(2);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(4);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(6);
+	REQUIRE(game.game_over_3() == true);
+	REQUIRE(game.get_winner_3() == "C"); // require c winner
 }
 
 TEST_CASE("Verify win by column executes properly when X is in postion 1, 4, 7")
 {
 	// Verify 3 X's in first column ends game with winner "X"
-	TicTacToe game;
+	TicTacToe3 game;
 	std::string first_player = "X";
-	game.start_game(first_player);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(1); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(2); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(4); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(5); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(7); // X
-	REQUIRE(game.game_over() == true);
-	REQUIRE(game.get_winner() == "X"); // require x winner
+	game.start_game_3(first_player);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(1); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(2); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(4); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(5); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(7); // X
+	REQUIRE(game.game_over_3() == true);
+	REQUIRE(game.get_winner_3() == "X"); // require x winner
 }
 
 TEST_CASE("Verify win by column executes properly when X is in postion 2, 5, 8")
 {
 	// Verify 3 X's in second column ends game with winner "X"
-	TicTacToe game;
+	TicTacToe3 game;
 	std::string first_player = "X";
-	game.start_game(first_player);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(2); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(1); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(5); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(4); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(8); // X
-	REQUIRE(game.game_over() == true);
-	REQUIRE(game.get_winner() == "X"); // require x winner
+	game.start_game_3(first_player);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(2); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(1); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(5); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(4); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(8); // X
+	REQUIRE(game.game_over_3() == true);
+	REQUIRE(game.get_winner_3() == "X"); // require x winner
 }
 
 TEST_CASE("Verify win by column executes properly when X is in postion 3, 6, 9")
 {
 	// Verify 3 X's in third column ends game with winner "X"
-	TicTacToe game;
+	TicTacToe3 game;
 	std::string first_player = "X";
-	game.start_game(first_player);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(3); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(1); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(6); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(4); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(9); // X
-	REQUIRE(game.game_over() == true);
-	REQUIRE(game.get_winner() == "X"); // require x winner
+	game.start_game_3(first_player);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(3); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(1); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(6); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(4); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(9); // X
+	REQUIRE(game.game_over_3() == true);
+	REQUIRE(game.get_winner_3() == "X"); // require x winner
 }
 
 TEST_CASE("Verify win by row executes properly when X is in postion 1, 2, 3")
 {
 	// Verify 3 X's in first row ends game with winner "X"
-	TicTacToe game;
+	TicTacToe3 game;
 	std::string first_player = "X";
-	game.start_game(first_player);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(1); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(4); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(2); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(5); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(3); // X
-	REQUIRE(game.game_over() == true);
-	REQUIRE(game.get_winner() == "X"); // require x winner
+	game.start_game_3(first_player);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(1); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(4); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(2); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(5); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(3); // X
+	REQUIRE(game.game_over_3() == true);
+	REQUIRE(game.get_winner_3() == "X"); // require x winner
 }
 
 TEST_CASE("Verify win by row executes properly when X is in postion 4, 5, 6")
 {
 	// Verify 3 X's in second row ends game with winner "X"
-	TicTacToe game;
+	TicTacToe3 game;
 	std::string first_player = "X";
-	game.start_game(first_player);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(4); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(1); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(5); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(2); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(6); // X
-	REQUIRE(game.game_over() == true);
-	REQUIRE(game.get_winner() == "X"); // require x winner
+	game.start_game_3(first_player);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(4); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(1); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(5); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(2); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(6); // X
+	REQUIRE(game.game_over_3() == true);
+	REQUIRE(game.get_winner_3() == "X"); // require x winner
 }
 
 TEST_CASE("Verify win by row executes properly when X is in postion 7, 8, 9")
 {
 	// Verify 3 X's in third row ends game with winner "X"
-	TicTacToe game;
+	TicTacToe3 game;
 	std::string first_player = "X";
-	game.start_game(first_player);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(7); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(1); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(8); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(2); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(9); // X
-	REQUIRE(game.game_over() == true);
-	REQUIRE(game.get_winner() == "X"); // require x winner
+	game.start_game_3(first_player);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(7); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(1); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(8); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(2); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(9); // X
+	REQUIRE(game.game_over_3() == true);
+	REQUIRE(game.get_winner_3() == "X"); // require x winner
 }
 
 TEST_CASE("Verify win by diagonal executes properly when X is in postion 1, 5, 9")
 {
 	// Verify 3 X's diagonal left to right ends game with winner "X"
-	TicTacToe game;
+	TicTacToe3 game;
 	std::string first_player = "X";
-	game.start_game(first_player);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(1); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(2); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(5); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(3); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(9); // X
-	REQUIRE(game.game_over() == true);
-	REQUIRE(game.get_winner() == "X"); // require x winner
+	game.start_game_3(first_player);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(1); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(2); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(5); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(3); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(9); // X
+	REQUIRE(game.game_over_3() == true);
+	REQUIRE(game.get_winner_3() == "X"); // require x winner
 }
 
 TEST_CASE("Verify win by diagonal executes properly when X is in postion 3, 5, 7")
 {
 	// Verify 3 X's diagonal right to left ends game with winner "X"
-	TicTacToe game;
+	TicTacToe3 game;
 	std::string first_player = "X";
-	game.start_game(first_player);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(3); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(1); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(5); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(2); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(7); // X
-	REQUIRE(game.game_over() == true);
-	REQUIRE(game.get_winner() == "X"); // require x winner
+	game.start_game_3(first_player);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(3); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(1); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(5); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(2); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(7); // X
+	REQUIRE(game.game_over_3() == true);
+	REQUIRE(game.get_winner_3() == "X"); // require x winner
 }
 
 TEST_CASE("Verify that x win counter increments properly")
 {
-	TicTacToe game;
-	TicTacToeManager mgr;
+	TicTacToe3 game;
 	std::string first_player = "X";
-	game.start_game(first_player);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(1); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(4); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(2); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(5); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(3); // X
-	REQUIRE(game.game_over() == true);
-	REQUIRE(game.get_winner() == "X"); // require x winner
-	mgr.save_game(game);
+	game.start_game_3(first_player);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(1); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(4); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(2); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(5); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(3); // X
+	REQUIRE(game.game_over_3() == true);
+	REQUIRE(game.get_winner_3() == "X"); // require x winner
 }
 
 TEST_CASE("Verify that o win counter increments properly")
 {
-	TicTacToe game;
-	TicTacToeManager mgr;
+	TicTacToe3 game;
 	std::string first_player = "O";
-	game.start_game(first_player);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(1); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(4); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(2); // X
-	REQUIRE(game.game_over() == false);
-	game.mark_board(5); // O
-	REQUIRE(game.game_over() == false);
-	game.mark_board(3); // X
-	REQUIRE(game.game_over() == true);
-	REQUIRE(game.get_winner() == "O"); // require O winner
-	mgr.save_game(game);
+	game.start_game_3(first_player);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(1); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(4); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(2); // X
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(5); // O
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(3); // X
+	REQUIRE(game.game_over_3() == true);
+	REQUIRE(game.get_winner_3() == "O"); // require O winner
 }
 
 TEST_CASE("Verify that ties counter increments properly")
 {
-	TicTacToe game;
-	TicTacToeManager mgr;
+	TicTacToe3 game;
 	std::string first_player = "X";
-	game.start_game(first_player);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(1);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(5);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(9);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(3);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(7);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(8);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(2);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(4);
-	REQUIRE(game.game_over() == false);
-	game.mark_board(6);
-	REQUIRE(game.game_over() == true);
-	REQUIRE(game.get_winner() == "C"); 
-	mgr.save_game(game);
+	game.start_game_3(first_player);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(1);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(5);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(9);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(3);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(7);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(8);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(2);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(4);
+	REQUIRE(game.game_over_3() == false);
+	game.mark_board_3(6);
+	REQUIRE(game.game_over_3() == true);
+	REQUIRE(game.get_winner_3() == "C"); // require c winner
 }
